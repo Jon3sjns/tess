ba01baa75db1c81ba622abc5a24d485616d4feef
Billy <45759837+Jon3sjns@users.noreply.github.com>
2019-06-02 23:10:22 +0700

    Add files via upload

---
+++ a.sh
@@ -0,0 +1,74 @@
+
+clear
+j="\e[37m{===============================================}"
+c='\e[36m'
+o="{===============================================}"
+echo $o|lolcat
+echo $o|lolcat
+echo $o|lolcat
+echo "                 TOOLS INSTALLER"
+echo $o|lolcat
+echo $o|lolcat
+echo $o|lolcat
+
+
+clear
+echo $j
+echo " 1.install RushScan"
+echo $j
+echo " 2.install HashSpam"
+echo $j
+echo " 3. install information gathering"
+echo $j
+echo " 4. install PHP-ROOTKIT"
+echo $j
+echo " 5. install MD5cak"
+echo $j
+echo 6. install file-attachment-exploit
+echo $j
+echo 7. install s1mspof
+echo $j
+echo 8. install PYSNIPER
+echo $j
+echo 9. install brute force wordpress
+echo $j
+echo " 10. install TroD0n(auto downloader)"
+echo $j
+echo " 11. install D1R5(Dir & Sensitive File Scanner)"
+echo $j
+echo 12. install HeaderCheckerV1.0
+echo $j
+echo " 13. install A5Inject(auto SQL Injection)"
+echo $j
+echo "14. install anti malware website"
+echo $j
+echo "15. install BruteForce FTP"
+echo $j
+echo "16. install XuDoS"
+echo $j
+echo 17. install PortScanner
+echo $j
+echo 18. install SQLMAP
+echo $j
+echo 19. install DARKXploit
+echo $j
+echo 20. install metasploit framework
+echo $j
+echo "21. install IPCS(hack kamera)"
+echo $j
+echo 22. install santet online
+echo $j
+echo 23. install Enskripsi base64
+echo $j
+echo 24. install deskripsi base64
+echo $j
+echo 25. install ContexPloit
+echo $j
+echo 26. install TH3INSPECTOR
+echo $j
+echo 27. menampilkan spek handphone
+echo $j
+echo 27. install
+echo $j
+
+
