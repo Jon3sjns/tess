9541cbc80692d571eded6de75e77345e2d7c30a4
Billy <45759837+Jon3sjns@users.noreply.github.com>
2019-06-02 23:09:33 +0700

    Add files via upload

--- tool.sh
+++ tool.sh
@@ -1,4 +1,5 @@
 
+clear
 j="\e[37m{===============================================}"
 c='\e[36m'
 o="{===============================================}"
@@ -13,37 +14,61 @@ echo $o|lolcat
 
 clear
 echo $j
-echo "$c 1.install RushScan"
+echo " 1.install RushScan"
 echo $j
-echo "$c 2.install HashSpam"
+echo " 2.install HashSpam"
 echo $j
-echo "$c 3. install information gathering"
+echo " 3. install information gathering"
 echo $j
-echo "$c 4. install PHP-ROOTKIT"
+echo " 4. install PHP-ROOTKIT"
 echo $j
-echo "$c 5. install MD5cak"
+echo " 5. install MD5cak"
 echo $j
-echo $c 6. install file-attachment-exploit
+echo 6. install file-attachment-exploit
 echo $j
-echo $c 7. install s1mspof
+echo 7. install s1mspof
 echo $j
-echo $c 8. install PYSNIPER
+echo 8. install PYSNIPER
 echo $j
-echo $c 9. install brute force wordpress
+echo 9. install brute force wordpress
 echo $j
-echo "$c 10. install TroD0n(auto downloader)"
+echo " 10. install TroD0n(auto downloader)"
 echo $j
-echo "$c 11. install D1R5(Dir & Sensitive File Scanner)"
+echo " 11. install D1R5(Dir & Sensitive File Scanner)"
 echo $j
-echo $c 12. install HeaderCheckerV1.0
+echo 12. install HeaderCheckerV1.0
 echo $j
-echo "$c 13. install A5Inject(auto SQL Injection)"
+echo " 13. install A5Inject(auto SQL Injection)"
 echo $j
-echo "$c 14. install anti malware website"
+echo "14. install anti malware website"
 echo $j
-echo "$c 15. install BruteForce FTP"
+echo "15. install BruteForce FTP"
 echo $j
-echo "$c 16. install XuDoS"
+echo "16. install XuDoS"
 echo $j
-echo $c 17. install PortScanner
+echo 17. install PortScanner
 echo $j
+echo 18. install SQLMAP
+echo $j
+echo 19. install DARKXploit
+echo $j
+echo 20. install metasploit framework
+echo $j
+echo "21. install IPCS(hack kamera)"
+echo $j
+echo 22. install santet online
+echo $j
+echo 23. install Enskripsi base64
+echo $j
+echo 24. install deskripsi base64
+echo $j
+echo 25. install ContexPloit
+echo $j
+echo 26. install TH3INSPECTOR
+echo $j
+echo 27. menampilkan spek handphone
+echo $j
+echo 27. install
+echo $j
+
+
